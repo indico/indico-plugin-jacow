cf90a848dabb719ed44d0f6e57094dd9325d988b
Pedro Lourenço <public@plourenco.com>
2022-05-24 15:18:08 +0100

    Simplify question ratings

--- indico_jacow/controllers.py
+++ indico_jacow/controllers.py
@@ -6,30 +6,43 @@ from indico.util.spreadsheets import send_csv, send_xlsx
 
 
 class RHAbstractsExportBase(RHManageAbstractsExportActionsBase):
-    def get_scores(self, abstract):
-        sums = sum((Counter(review.scores) for review in abstract.reviews), Counter())
-        lens = sum((Counter(review.scores.keys()) for review in abstract.reviews), Counter())
-        return {question: value / lens[question] for question, value in sums.items()}
+    def get_ratings(self, abstract):
+        result = defaultdict(list)
+        for review in abstract.reviews:
+            for rating in review.ratings:
+                result[rating.question].append(rating)
+        return result
 
     def _generate_spreadsheet(self):
         export_config = self.list_generator.get_list_export_config()
         field_names, rows = generate_spreadsheet_from_abstracts(self.abstracts, export_config['static_item_ids'],
                                                                 export_config['dynamic_items'])
-        for idx, abstract in enumerate(self.abstracts):
-            for question, score in self.get_scores(abstract).items():
-                title = f'Question {question.title} (AVG score)'
-                field_names.append(title)
-                rows[idx][title] = round(score, 1)
-            questions = defaultdict(list)
-            for review in abstract.reviews:
-                for rating in review.ratings:
-                    if rating.question.field_type == 'bool':
-                        questions[rating.question].append(rating.value)
-            for question, values in questions.items():
+
+        def get_score_column(title):
+            return f'Question {title} (AVG score)'
+
+        def get_count_column(title, value):
+            return f'Question {title} ({str(value)})'
+
+        questions = [question for question in self.event.abstract_review_questions if not question.is_deleted]
+        for question in questions:
+            field_names.append(f'Question {question.title} (AVG score)')
+            if question.field_type == 'bool':
                 for answer in [True, False, None]:
-                    title = f'Question {question.title} ({str(answer)})'
-                    field_names.append(title)
-                    rows[idx][title] = len([v for v in values if v == answer])
+                    field_names.append(f'Question {question.title} ({str(answer)})')
+
+        for idx, abstract in enumerate(self.abstracts):
+            ratings = self.get_ratings(abstract)
+            for question in questions:
+                scores = [r for r in ratings.get(question, [])
+                          if not r.question.no_score and r.value is not None]
+                rows[idx][get_score_column(question.title)] = round(sum(r.value for r in scores) /
+                                                                    len(scores), 1) if scores else 0
+                if question.field_type == 'bool':
+                    for answer in [True, False, None]:
+                        count = len([v for v in ratings.get(question, []) if v == answer])
+                        rows[idx][get_count_column(question.title, answer)] = count
+
         return field_names, rows
 
 
