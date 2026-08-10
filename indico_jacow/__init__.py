7523a3540f24a7387db311973e1d133cd3cd1184
Adrian Moennich <adrian.moennich@cern.ch>
2026-08-10 11:47:59 +0200

    Remove obsolete profile sync

--- indico_jacow/__init__.py
+++ indico_jacow/__init__.py
@@ -4,10 +4,3 @@
 # The CERN Indico plugins are free software; you can redistribute
 # them and/or modify them under the terms of the MIT License; see
 # the LICENSE file for more details.
-
-from indico.core import signals
-
-
-@signals.core.import_tasks.connect
-def _import_tasks(sender, **kwargs):
-    import indico_jacow.task  # noqa: F401
--- indico_jacow/plugin.py
+++ indico_jacow/plugin.py
@@ -60,12 +60,10 @@ REPO_MANAGER_RHS = (
 
 class SettingsForm(IndicoForm):
     _fieldsets = [
-        (_('General'), ('sync_enabled', 'brevo_api_key')),
+        (_('General'), ('brevo_api_key',)),
         (_('User profile management'), ('repo_managers',)),
     ]
 
-    sync_enabled = BooleanField(_('Sync profiles'), widget=SwitchWidget(),
-                                description=_('Periodically sync user details with the central database'))
     repo_managers = PrincipalListField(_('Central Repo Managers'), allow_groups=True,
                                        description=_('List of users who can manage Indico user profiles without being '
                                                      'full Indico admins'))
@@ -81,7 +79,6 @@ class JACOWPlugin(IndicoPlugin):
     configurable = True
     settings_form = SettingsForm
     default_settings = {
-        'sync_enabled': False,
         'brevo_api_key': '',
     }
     acl_settings = {
